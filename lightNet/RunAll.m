35d4ba0fa25c83c262b8970e3b396dcfb902ed56
andrew172839 <yushao.cheng@gmail.com>
2017-10-22 17:42:51 +0800

    .

--- lightNet/RunAll.m
+++ lightNet/RunAll.m
@@ -1,22 +1,22 @@
-clear all;
-cd('./MLP');
-disp('Testing training a multilayer perceptron.')
-Main_MNIST_MLP_RMSPROP();
-cd ..
-
-clear all;
-
-cd('./ReinforcementLearning');
-disp('Testing training a Q-network.')
-Main_Cart_Pole_Q_Network
-cd ..
-
-
-clear all;
-cd('./RNN');
-disp('Testing training an LSTM.')
-Main_Char_RNN();
-cd ..
+% clear all;
+% cd('./MLP');
+% disp('Testing training a multilayer perceptron.')
+% Main_MNIST_MLP_RMSPROP();
+% cd ..
+% 
+% clear all;
+% 
+% cd('./ReinforcementLearning');
+% disp('Testing training a Q-network.')
+% Main_Cart_Pole_Q_Network
+% cd ..
+% 
+% 
+% clear all;
+% cd('./RNN');
+% disp('Testing training an LSTM.')
+% Main_Char_RNN();
+% cd ..
 
 clear all;
 cd('./CNN');
