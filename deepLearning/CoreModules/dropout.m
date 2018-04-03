9f6e944cd5ace3be095c632074bcde4bf3d207a2
andrew789 <yushao.cheng@gmail.com>
2018-04-03 13:45:14 +0800

    .

--- deepLearning/CoreModules/dropout.m
+++ deepLearning/CoreModules/dropout.m
@@ -6,16 +6,16 @@ scale = cast(1 / (1 - opts.rate), 'like', x) ;
 
 backMode = 1;
 if isempty(mask)
-	backMode = 0;
-	if isa(x, 'gpuArray')
-		mask = scale * (gpuArray.rand(size(x), classUnderlying(x)) >= opts.rate);
-	else
-		mask = scale * (rand(size(x), 'like', x) >= opts.rate);
-	end
+    backMode = 0;
+    if isa(x, 'gpuArray')
+        mask = scale * (gpuArray.rand(size(x), classUnderlying(x)) >= opts.rate);
+    else
+        mask = scale * (rand(size(x), 'like', x) >= opts.rate);
+    end
 end
 
 if ~backMode
-	y = mask .* x ;   
+    y = mask .* x ;
 else
-	y = mask .* dzdy ;   
+    y = mask .* dzdy ;
 end
